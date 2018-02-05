fb8fa0a694a433d865a2a028fd35a0f9606d6bf7
Marin Pepeljnjak <marin.pepeljnjak@vsite.hr>
2018-02-05 23:35:56 +0100

    napravljeno uz pomoc pouzdanih izvora

--- WorldTimeCSH/Form1.Designer.cs
+++ WorldTimeCSH/Form1.Designer.cs
@@ -30,11 +30,9 @@
         {
             this.components = new System.ComponentModel.Container();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
-            this.label1 = new System.Windows.Forms.Label();
-            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // timer1
@@ -43,66 +41,42 @@
             this.timer1.Interval = 250;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(22, 51);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(29, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "UTC";
-            // 
-            // numericUpDown1
-            // 
-            this.numericUpDown1.Location = new System.Drawing.Point(57, 48);
-            this.numericUpDown1.Maximum = new decimal(new int[] {
-            12,
-            0,
-            0,
-            0});
-            this.numericUpDown1.Minimum = new decimal(new int[] {
-            12,
-            0,
-            0,
-            -2147483648});
-            this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(58, 20);
-            this.numericUpDown1.TabIndex = 1;
-            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
-            // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 115);
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox1.Location = new System.Drawing.Point(55, 146);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(260, 20);
+            this.textBox1.Size = new System.Drawing.Size(283, 30);
             this.textBox1.TabIndex = 2;
             // 
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Items.AddRange(new object[] {
-            "hr-HR",
-            "en-GB",
-            "en-US",
-            "ja-JP"});
-            this.comboBox1.Location = new System.Drawing.Point(130, 47);
+            this.comboBox1.Location = new System.Drawing.Point(12, 80);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.Size = new System.Drawing.Size(97, 21);
             this.comboBox1.TabIndex = 3;
             this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(12, 53);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(380, 21);
+            this.comboBox2.TabIndex = 4;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(404, 261);
+            this.Controls.Add(this.comboBox2);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.numericUpDown1);
-            this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -111,10 +85,9 @@
         #endregion
 
         private System.Windows.Forms.Timer timer1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.NumericUpDown numericUpDown1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
     }
 }
 
--- WorldTimeCSH/Form1.cs
+++ WorldTimeCSH/Form1.cs
@@ -13,32 +13,37 @@ namespace WorldTimeCSH
 {
     public partial class Form1 : Form
     {
-        int coefficient = -1;
         CultureInfo cultureInfo = new CultureInfo("hr-HR");
+        TimeSpan ts;
 
         public Form1()
         {
             InitializeComponent();
-        }
-
-        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
-        {
-            coefficient =(int)numericUpDown1.Value-1;
+            comboBox1.Items.AddRange(CultureInfo.GetCultures(CultureTypes.AllCultures));
+            comboBox2.Items.AddRange(TimeZoneInfo.GetSystemTimeZones().ToArray<TimeZoneInfo>());
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
             DateTime current;
             int hourHelper = DateTime.Now.Hour;
-            hourHelper += coefficient;
-            current = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, hourHelper, DateTime.Now.Minute, DateTime.Now.Second, cultureInfo.Calendar);
-            textBox1.Text = current.ToLongDateString()+", "+current.ToLongTimeString();
+            hourHelper +=ts.Hours;
+            hourHelper %= 24;
+            if (hourHelper < 0)
+                hourHelper = 24 + hourHelper;
+            current = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, hourHelper, DateTime.Now.Minute, DateTime.Now.Second);
+            textBox1.Text = current.ToString(cultureInfo);
         }
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            cultureInfo = new CultureInfo(comboBox1.SelectedText);
+            cultureInfo = (CultureInfo)comboBox1.SelectedItem;
         }
 
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            TimeZoneInfo tzi = (TimeZoneInfo)comboBox2.SelectedItem;
+            ts = tzi.BaseUtcOffset;
+        }
     }
 }
