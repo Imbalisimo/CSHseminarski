476fd244817d47a71e2b59ea50b5ed58df417837
Marin Pepeljnjak <marin.pepeljnjak@vsite.hr>
2018-02-06 14:31:04 +0100

    editing disabled

--- WorldTimeCSH/Form1.Designer.cs
+++ WorldTimeCSH/Form1.Designer.cs
@@ -51,6 +51,7 @@
             // 
             // comboBox1
             // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox1.FormattingEnabled = true;
             this.comboBox1.Location = new System.Drawing.Point(12, 80);
             this.comboBox1.Name = "comboBox1";
@@ -60,6 +61,7 @@
             // 
             // comboBox2
             // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox2.FormattingEnabled = true;
             this.comboBox2.Location = new System.Drawing.Point(12, 53);
             this.comboBox2.Name = "comboBox2";
